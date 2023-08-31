b0000b007b5418b6462e541e4c098c392bcde7ac
praneeth-josyula <praneeth1405@gmail.com>
2023-08-30 19:00:58 -0600

    Initial Commit

---
+++ assignment1/assignment1App.swift
@@ -0,0 +1,17 @@
+//
+//  assignment1App.swift
+//  assignment1
+//
+//  Created by J V S Dattu on 8/30/23.
+//
+
+import SwiftUI
+
+@main
+struct assignment1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
